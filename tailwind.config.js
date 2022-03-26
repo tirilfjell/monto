8622b8a72c0ea5f7ec7db5b1892c8ca9fdb6078e
Tiril <tiril@ellera.no>
2022-03-26 13:52:03 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+// tailwind.config.js
+const colors = require('tailwindcss/colors')
+
+module.exports = {
+  purge: [],
+  darkMode: 'class',
+  theme: {
+    theme: {
+      extend: {
+        colors: {
+          teal: colors.teal,
+          cyan: colors.cyan,
+        },
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/aspect-ratio'),
+  ],
+}
